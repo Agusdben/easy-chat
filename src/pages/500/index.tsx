6015a91017ed14ef8bc1a6ac87aac960a20c771e
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-04-04 10:47:14 -0300

    Made 500 page protected

--- src/pages/500/index.tsx
+++ src/pages/500/index.tsx
@@ -1,7 +1,20 @@
+import useSocket from '@/hooks/useSocket'
 import Error from 'next/error'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 const Custom500: React.FC = () => {
+  const { isConnected } = useSocket()
+  const router = useRouter()
+
+  if (isConnected === undefined) return <></>
+
+  if (isConnected === true) {
+    router.push('/')
+      .catch(error => { console.error(error) })
+    return <></>
+  }
+
   return (
     <Error statusCode={500} title='Internal server error'/>
   )
