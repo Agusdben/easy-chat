10cdf11d615aac598cbdd070792089091b95bab2
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-04-18 12:16:26 -0300

    added link back to home

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -3,12 +3,15 @@ import Logo from '../Logo'
 import UserIcon from '../Icons/UserIcon'
 import useUser from '@/hooks/useUser'
 import styles from './AppHeader.module.css'
+import Link from 'next/link'
 
 const AppHeader: React.FC = () => {
   const { user } = useUser()
   return (
     <header className={styles.header}>
-      <Logo size='normal' />
+      <Link href='/'>
+        <Logo size='normal' />
+      </Link>
       {
         (user !== null) && (
       <section className={styles.user_section}>
